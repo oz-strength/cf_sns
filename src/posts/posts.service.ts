4a43ef6924deaf3d5e9af3c5d204d12824dbbb34
seungwoo <sw6016@hanmail.net>
2026-04-18 21:31:04 +0900

    FindOne 함수 이용해서 하나의 데이터만 찾기

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -58,8 +58,13 @@ export class PostsService {
     return this.postsRepository.find();
   }
 
-  getPostById(id: number) {
-    const post = posts.find((post) => post.id === +id);
+  async getPostById(id: number) {
+    const post = await this.postsRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
     if (!post) {
       throw new NotFoundException();
     }
