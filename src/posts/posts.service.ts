722e4aabdfffb575636ebde02b48ffb901ca1c54
seungwoo <sw6016@hanmail.net>
2026-04-18 21:39:41 +0900

    Create 함수로 새로운 데이터 생성하기

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -71,19 +71,20 @@ export class PostsService {
     return post;
   }
 
-  createPost(author: string, title: string, content: string) {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
+  async createPost(author: string, title: string, content: string) {
+    // 1) create -> 저장할 객체를 생성한다.
+    // 2) save -> 실제로 데이터베이스에 저장한다.
+
+    const post = this.postsRepository.create({
       author,
       title,
       content,
       likeCount: 0,
       commentCount: 0,
-    };
+    });
 
-    posts = [...posts, post];
-
-    return post;
+    const newPost = await this.postsRepository.save(post);
+    return newPost;
   }
 
   updatePost(
