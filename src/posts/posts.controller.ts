6e017b69b569a9e8f471fb371b6f665c52e9e2f4
seungwoo <sw6016@hanmail.net>
2026-04-18 12:27:59 +0900

    get posts 구현하기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -9,7 +9,8 @@ import { PostsService } from './posts.service';
  * commentCount: number;
  */
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -17,18 +18,53 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'John Doe',
+    title: 'Hello World',
+    content: 'This is my first post!',
+    likeCount: 100,
+    commentCount: 20,
+  },
+  {
+    id: 2,
+    author: 'Jane Smith',
+    title: 'My Second Post',
+    content: 'This is another post.',
+    likeCount: 50,
+    commentCount: 10,
+  },
+  {
+    id: 3,
+    author: 'Bob Johnson',
+    title: 'My Third Post',
+    content: 'This is my third post.',
+    likeCount: 75,
+    commentCount: 15,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1. GET /posts
+  // 모든 post를 반환하는 API
   @Get()
-  getPost(): Post {
-    return {
-      author: 'John Doe',
-      title: 'Hello World',
-      content: 'This is my first post!',
-      likeCount: 100,
-      commentCount: 20,
-    };
+  getPosts(): PostModel[] {
+    return posts;
   }
+
+  // 2. GET /posts/:id
+  // id에 해당하는 post를 반환하는 API
+
+  // 3. POST /posts
+  // 새로운 post를 생성하는 API
+
+  // 4. PUT /posts/:id
+  // id에 해당하는 post를 업데이트하는 API
+
+  // 5. DELETE /posts/:id
+  // id에 해당하는 post를 삭제하는 API
 }
