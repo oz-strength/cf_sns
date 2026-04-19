7d3e5577dd9a60d10aa600f73a7fe66892e77219
seungwoo <sw6016@hanmail.net>
2026-04-19 19:37:22 +0900

    User 관련 서비스 및 컨트롤러 기능 작성하기

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModel } from './entities/users.entity';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsersModel])], // Repository를 DI 컨테이너에 등록
   controllers: [UsersController],
   providers: [UsersService],
 })
