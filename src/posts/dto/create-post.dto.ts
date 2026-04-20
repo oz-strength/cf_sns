bd12c00b09a5a20cdcece7695c894aa150c8f85d
seungwoo <sw6016@hanmail.net>
2026-04-20 15:56:34 +0900

    Class Validator 에러 메세지 변경하기

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,9 +1,13 @@
 import { IsString } from 'class-validator';
 
 export class CreatePostDto {
-  @IsString()
+  @IsString({
+    message: 'title은 string 타입을 입력 해줘야합니다.',
+  })
   title: string;
 
-  @IsString()
+  @IsString({
+    message: 'content는 string타입을 입력 해줘야합니다.',
+  })
   content: string;
 }
