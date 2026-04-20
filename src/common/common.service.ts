0d16c9803747d972081b3cd565e7419f695b6948
seungwoo <sw6016@hanmail.net>
2026-04-20 20:49:51 +0900

    작업할 Pagination 로직 정리하기

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -10,5 +10,63 @@ export class CommonService {
     repository: Repository<T>,
     overrideFindOptions: FindManyOptions<T> = {},
     path: string,
+  ) {
+    if (dto.page) {
+      return this.pagePaginate(dto, repository, overrideFindOptions);
+    } else {
+      return this.cursorPaginate(dto, repository, overrideFindOptions, path);
+    }
+  }
+
+  private async pagePaginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
   ) {}
+
+  private async cursorPaginate<T extends BaseModel>(
+    dto: BasePaginationDto,
+    repository: Repository<T>,
+    overrideFindOptions: FindManyOptions<T> = {},
+    path: string,
+  ) {
+    /**
+     * where__likeCount__more_than
+     *
+     * whre__title__ilike
+     */
+  }
+
+  composeFindOptions<T extends BaseModel>(
+    dto: BasePaginationDto,
+  ): FindManyOptions<T> {
+    /**
+     * where,
+     * order,
+     * take,
+     * skip -> page 기반 pagination에서만 사용
+     */
+    /**
+     * DTO의 현재 생긴 구조는 아래와 같다
+     *
+     * {
+     *   where__id__more_than: 1;
+     *   order__createdAt: 'ASC';
+     * }
+     *
+     * 현재는 where__id__more_than, where__id__less_than 에 해당되는 where 필터만 사용중이지만
+     * 향후에 where__likeCount__more_than, where__title__ilike 등 다양한 필터가 추가될 수 있다.
+     * 모든 where 필터들을 자동으로 파싱할 수 있을만한 기능을 제작해야한다.
+     *
+     * 1) where로 시작한다면 필터 로직을 적용한다.
+     * 2) order로 시작한다면 정렬 로직을 적용한다.
+     * 3) 필터 로직을 적용한다면 '__' 기준으로 split 했을 때 3개의 값으로 나뉘는지
+     *    2개의 값으로 나뉘는지 확인한다.
+     * 3-1) 3개의 값으로 나뉜다면 FILTER_MAPPER에서 해당되는 operator 함수를 찾아서 적용한다.
+     *      ['where', 'id', 'more_than'] -> MoreThan(dto['where__id__more_than'])
+     * 3-2) 2개의 값으로 나뉜다면 정확한 값을 필터하는 것이기 때문에 operator 없이 적용한다.
+     *      ['where', 'id'] -> dto['where__id']
+     * 4) order의 경우 3-2)와 동일하게 적용한다.
+     */
+  }
 }
