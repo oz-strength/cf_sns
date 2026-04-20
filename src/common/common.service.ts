f14bb8d4d15bcaf9454a19e49ad91cf8a2c4d52c
seungwoo <sw6016@hanmail.net>
2026-04-20 21:24:46 +0900

    ParseWhereFilter 작업하기

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import {
   FindManyOptions,
   FindOptionsOrder,
   FindOptionsWhere,
   Repository,
 } from 'typeorm';
+import { FILTER_MAPPER } from './const/filter-mapper.const';
 import { BasePaginationDto } from './dto/base-pagination.dto';
 import { BaseModel } from './entity/base.entity';
 
@@ -104,7 +105,74 @@ export class CommonService {
   private parseWhereFilter<T extends BaseModel>(
     key: string,
     value: any,
-  ): FindOptionsWhere<T> {}
+  ): FindOptionsWhere<T> {
+    const options: FindOptionsWhere<T> = {};
+
+    /**
+     * 예를 들어 where__id__more_than 이라는 key가 들어온다면
+     * 1) key를 '__' 기준으로 split 한다 -> ['where', 'id', 'more_than']
+     */
+    const split = key.split('__');
+
+    if (split.length !== 2 && split.length !== 3) {
+      throw new BadRequestException(
+        `where 필터는 '__' 기준으로 2개 또는 3개의 값으로 나뉘어야 합니다. 현재 key: ${key}`,
+      );
+    }
+
+    /**
+     * 길이가 2일 경우는
+     * where__id = 3
+     *
+     * FindOptionsWhere로 풀어보면
+     * 아래와 같다
+     *
+     * {
+     *   id: 3,
+     * }
+     */
+    if (split.length === 2) {
+      // ['where', 'id']
+      const [_, field] = split;
+
+      /**
+       * field: id
+       * value: 3
+       */
+      options[field] = value;
+    } else {
+      /**
+       * 길이가 3일 경우에는 TypeORM의 유틸리티 적용이 필요한 경우이다.
+       *
+       * where__id__more_than의 경우
+       * where 는 버려도 되고 두번째 값인 id는 필터할 키값이 되고
+       * 세번째 값은 more_than이며 TypeORM의 유틸리티가 된다.
+       *
+       * FILTER_MAPPER에서 미리 정의해둔 값들로
+       * field 값에 FILTER_MAPPER에서 해당되는 utility를 가져온 후
+       * 값에 적용해 준다.
+       */
+
+      // ['where', 'id', 'more_than']
+      const [_, field, operator] = split;
+
+      // where__id__between = 3,4
+      // 만약에 split 대상 문자가 존재하지 않으면 길이가 무조건 1이다.
+      // const values = value.toString().split(',');
+
+      // field: id
+      // operator: more_than
+      // FILTER_MAPPER[more_than] -> MoreThan
+      // if (operator === 'between') {
+      //   options[field] = FILTER_MAPPER[operator](values[0], values[1]);
+      // } else {
+      //   options[field] = FILTER_MAPPER[operator](value);
+      // }
+      options[field] = FILTER_MAPPER[operator](value);
+    }
+
+    return options;
+  }
 
   private parseOrderFilter<T extends BaseModel>(
     key: string,
