5f550c3ae839a30294435188275b6efe35a4a67c
seungwoo <sw6016@hanmail.net>
2026-04-20 11:55:30 +0900

    여러개의 파이프 동시에 사용하기

--- src/auth/pipe/password.pipe.ts
+++ src/auth/pipe/password.pipe.ts
@@ -14,3 +14,34 @@ export class PasswordPipe implements PipeTransform {
     return value;
   }
 }
+
+@Injectable()
+export class MaxLengthPipe implements PipeTransform {
+  constructor(
+    private readonly length: number,
+    private readonly subject: string,
+  ) {}
+
+  transform(value: string, metadata: ArgumentMetadata) {
+    if (value.length > this.length) {
+      throw new BadRequestException(
+        `${this.subject}의 최대 길이는 ${this.length}입니다.`,
+      );
+    }
+
+    return value;
+  }
+}
+
+@Injectable()
+export class MinLengthPipe implements PipeTransform {
+  constructor(private readonly length: number) {}
+
+  transform(value: string, metadata: ArgumentMetadata) {
+    if (value.length < this.length) {
+      throw new BadRequestException(`최소 길이는 ${this.length}입니다.`);
+    }
+
+    return value;
+  }
+}
