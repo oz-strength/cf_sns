0685c960763aa47ed649cea92e504c1e0789915d
seungwoo <sw6016@hanmail.net>
2026-04-21 14:41:13 +0900

    Socket에 사용자 정보 저장하기

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,9 +1,4 @@
-import {
-  UseFilters,
-  UseGuards,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
+import { UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
@@ -14,9 +9,10 @@ import {
   WsException,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { SocketBearerTokenGuard } from 'src/auth/guard/socket/socket-bearer-token.guard';
+import { AuthService } from 'src/auth/auth.service';
 import { SocketCatchHttpExceptionFilter } from 'src/common/exception-filter/socket-catch-http.exception-filter';
 import { UsersModel } from 'src/users/entities/users.entity';
+import { UsersService } from 'src/users/users.service';
 import { ChatsService } from './chats.service';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { EnterChatDto } from './dto/enter-chat.dto';
@@ -31,13 +27,42 @@ export class ChatsGateway implements OnGatewayConnection {
   constructor(
     private readonly chatsService: ChatsService,
     private readonly messagesService: ChatsMessagesService,
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
   ) {}
 
   @WebSocketServer()
   server: Server;
 
-  handleConnection(socket: Socket) {
+  async handleConnection(socket: Socket & { user?: UsersModel }) {
     console.log(`on connection called : ${socket.id}`);
+
+    const headers = socket.handshake.headers;
+
+    // Bearer xxxx
+    const rawToken = headers.authorization;
+
+    if (!rawToken) {
+      socket.disconnect();
+      return;
+    }
+
+    try {
+      const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+      const payload = this.authService.verifyToken(token);
+      const user = await this.usersService.getUserByEmail(payload.email);
+
+      if (!user) {
+        throw new WsException('유저가 존재하지 않습니다');
+      }
+
+      socket.user = user;
+
+      return true;
+    } catch (e) {
+      socket.disconnect();
+    }
   }
 
   @UsePipes(
@@ -51,7 +76,6 @@ export class ChatsGateway implements OnGatewayConnection {
     }),
   )
   @UseFilters(SocketCatchHttpExceptionFilter)
-  @UseGuards(SocketBearerTokenGuard)
   @SubscribeMessage('create_chat')
   async createChat(
     @MessageBody() data: CreateChatDto,
@@ -61,7 +85,6 @@ export class ChatsGateway implements OnGatewayConnection {
   }
 
   @SubscribeMessage('enter_chat')
-  @UseGuards(SocketBearerTokenGuard)
   @UsePipes(
     new ValidationPipe({
       transform: true, // 값을 넣지 않아도 default 값이 형성되게 설정
@@ -76,7 +99,7 @@ export class ChatsGateway implements OnGatewayConnection {
   async enterChat(
     // 방의 chat ID들을 리스트로 받는다.
     @MessageBody() data: EnterChatDto,
-    @ConnectedSocket() socket: Socket,
+    @ConnectedSocket() socket: Socket & { user: UsersModel },
   ) {
     for (const chatId of data.chatIds) {
       const exists = await this.chatsService.checkIfChatExists(chatId);
@@ -94,7 +117,6 @@ export class ChatsGateway implements OnGatewayConnection {
 
   // socket.on('send_message', (message) => {console.log(message)});
   @SubscribeMessage('send_message')
-  @UseGuards(SocketBearerTokenGuard)
   @UsePipes(
     new ValidationPipe({
       transform: true, // 값을 넣지 않아도 default 값이 형성되게 설정
