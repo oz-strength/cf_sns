6039aeaef5c1a82986468644b5eaae4bf28c5e51
seungwoo <sw6016@hanmail.net>
2026-04-21 18:14:14 +0900

    Patch Comment API 작업하기

--- src/posts/comments/comments.service.ts
+++ src/posts/comments/comments.service.ts
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 import { DEFAULT_COMMENT_FIND_OPTIONS } from './const/default-comment-find-options.const';
 import { CreateCommentsDto } from './dto/create-comments.dto';
 import { PaginateCommentsDto } from './dto/paginate-comments.dto';
+import { UpdateCommentsDto } from './dto/update-comments.dto';
 import { CommentsModel } from './entity/comments.entity';
 
 @Injectable()
@@ -54,5 +55,22 @@ export class CommentsService {
       },
       author,
     });
+  }
+
+  async updateComment(dto: UpdateCommentsDto, commentId: number) {
+    const prevComment = await this.commentsRepository.preload({
+      id: commentId,
+      ...dto,
+    });
+
+    if (!prevComment) {
+      throw new BadRequestException(
+        `id: ${commentId}인 댓글이 존재하지 않습니다.`,
+      );
+    }
+
+    const newComment = await this.commentsRepository.save(prevComment);
+
+    return newComment;
   }
 }
