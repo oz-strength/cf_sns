291afa466194f646c3ddc681da116e015f14ab92
seungwoo <sw6016@hanmail.net>
2026-04-21 18:47:26 +0900

    PostExists Middleware 를  CommentsController에 적용하기

--- src/posts/comments/comments.module.ts
+++ src/posts/comments/comments.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { CommonModule } from 'src/common/common.module';
 import { UsersModule } from 'src/users/users.module';
+import { PostsModule } from '../posts.module';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 import { CommentsModel } from './entity/comments.entity';
+import { PostExistsMiddleware } from './middleware/post-exists.middleware';
 
 @Module({
   imports: [
@@ -13,8 +15,13 @@ import { CommentsModel } from './entity/comments.entity';
     CommonModule,
     AuthModule,
     UsersModule,
+    PostsModule,
   ],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
-export class CommentsModule {}
+export class CommentsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(PostExistsMiddleware).forRoutes(CommentsController);
+  }
+}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -54,5 +54,6 @@ import { PostsService } from './posts.service';
   ],
   controllers: [PostsController],
   providers: [PostsService, PostsImagesService],
+  exports: [PostsService],
 })
 export class PostsModule {}
